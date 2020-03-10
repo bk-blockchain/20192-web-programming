7fe1d59a579f283a13438b762d8611ff2483fc19
Hoang Minh Giap <santaclaushg@gmail.com>
2020-03-10 10:01:53 +0700

    Add homework lab1

---
+++ Exercise_5.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercise 5</title>
+        <script src="https://kit.fontawesome.com/yourcode.js"></script>
+        <link rel="stylesheet" href="exercise_5.css" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" type="text/css">
+    </head>
+    <body>
+        <div style="width:100%; display: flex; justify-content: center;">
+        <table>
+        <tr>
+            <th colspan="2" class="header">
+                <h3>Hoàng Minh Giáp</h3>
+                <p>Personal Detail: I am a 4th-year student at HUST. My major is Information and Communication technology</p>
+            </th>
+
+        </tr>
+        <tr>
+            <td class="row border-right" style="width: 200px">Education process</td>
+            <td colspan="2" class="row">Studying</td>
+        </tr>
+        <tr>
+          <td class="row border-right small">Working process</td>
+          <td colspan="2" class="row">Internship</td>
+        </tr>
+        <tr>
+            <td class="row border-right" style="width: 200px">Society activities</td>
+            <td colspan="2" class="row">
+                <ol>
+                    <li><i class="fab fa-facebook-square"></i> <a style="text-decoration: none;" href="https://www.facebook.com/minhgiap.97">Facebook</a></li>
+                    <li><i class="fab fa-linkedin"></i> <a style="text-decoration: none;" href="https://www.linkedin.com/in/giap-minh-6b713a141/">Linkedin</a></li>
+                </ol>
+               
+            </td>
+        </tr>
+        <tr>
+          <td class="row border-right small">Skills</td>
+          <td colspan="2" class="row">
+              <ul>
+                  <li>HTML/CSS</li>
+                  <li><em>JavaScript</em></li>
+                  <li><i>Java</i></li>
+                  <li><b>Python</b></li>
+              </ul>
+          </td>
+        </tr>
+        <tr>
+          <td class="row border-right small">Certifications</td>
+          <td colspan="2" class="row">
+              <img src="ML.png" style="height: 400px; width: 600px" />
+          </td>
+        </tr>
+        <tr>
+            <td class="row border-right" style="width: 200px">Hobbies</td>
+            <td colspan="2" class="row">Movie, Book, Guitar, Sing, Code</td>
+        </tr>
+        <tr>
+          <td class="row border-right small">Desire</td>
+          <td colspan="2" class="row">Learn more new insteresting things</td>
+        </tr>
+      </table>
+       </div>
+    </body>
+</html>
