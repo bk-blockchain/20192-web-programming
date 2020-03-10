918d0d00628716f299e1469b76430c34c6829bf3
Harry <harry@onlinebizsoft.com>
2020-03-10 09:43:40 +0700

    Expression

---
+++ week2/expression.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Expression</title>
+    </head>
+    <body>
+        <?php
+            $gr1 = 50;
+            $gr2 = 100;
+            $gr3 = 75;
+            $aver = ($gr1 + $gr2 + $gr3) / 3;
+            print "The average is $aver";
+        ?>
+    </body>
+</html>
