f843ff6f8c79705234be06d64d9af6814203413e
chung-pi <chung.dao88@gmail.com>
2020-05-19 09:27:19 +0700

    Fix bug in sqlquery

--- application/controllers/itemscontroller.php
+++ application/controllers/itemscontroller.php
@@ -18,12 +18,12 @@ class ItemsController extends Controller {
 	function add() {
 		$todo = $_POST['todo'];
 		$this->set('title','Success - My Todo List App');
-		$this->set('todo',$this->Item->query('insert into items (item_name) values (\''.mysql_real_escape_string($todo).'\')'));	
+		$this->set('todo',$this->Item->query('insert into items (item_name) values (\''.mysqli_real_escape_string($this->Item->_dbHandle, $todo).'\')'));	
 	}
 	
 	function delete($id = null) {
 		$this->set('title','Success - My Todo List App');
-		$this->set('todo',$this->Item->query('delete from items where id = \''.mysql_real_escape_string($id).'\''));	
+		$this->set('todo',$this->Item->query('delete from items where id = \''.mysqli_real_escape_string($this->Item->_dbHandle, $id).'\''));	
 	}
 
 }
--- library/sqlquery.class.php
+++ library/sqlquery.class.php
@@ -1,97 +1,88 @@
 <?php
 
 class SQLQuery {
-    protected $_dbHandle;
+
+    public $_dbHandle;
     protected $_result;
 
-    /** Connects to database **/
-
+    /** Connects to database * */
     function connect($address, $account, $pwd, $name) {
         $this->_dbHandle = @mysqli_connect($address, $account, $pwd);
         if ($this->_dbHandle) {
-            if (mysqli_select_db($name, $this->_dbHandle)) {
+            if (mysqli_select_db($this->_dbHandle, $name)) {
                 return 1;
-            }
-            else {
+            } else {
                 return 0;
             }
-        }
-        else {
+        } else {
             return 0;
         }
     }
 
-    /** Disconnects from database **/
-
+    /** Disconnects from database * */
     function disconnect() {
         if (@mysqli_close($this->_dbHandle) != 0) {
             return 1;
-        }  else {
+        } else {
             return 0;
         }
     }
-    
+
     function selectAll() {
-    	$query = 'select * from `'.$this->_table.'`';
-    	return $this->query($query);
+        $query = 'select * from `' . $this->_table . '`';
+        return $this->query($query);
     }
-    
+
     function select($id) {
-    	$query = 'select * from `'.$this->_table.'` where `id` = \''.mysqli_real_escape_string($id).'\'';
-    	return $this->query($query, 1);    
+        $query = 'select * from `' . $this->_table . '` where `id` = \'' . mysqli_real_escape_string($this->_dbHandle, $id) . '\'';
+        return $this->query($query, 1);
     }
 
-	
-    /** Custom SQL Query **/
+    /** Custom SQL Query * */
+    function query($query, $singleResult = 0) {
 
-	function query($query, $singleResult = 0) {
+        $this->_result = mysqli_query($this->_dbHandle, $query);
 
-		$this->_result = mysqli_query($query, $this->_dbHandle);
+        if (preg_match("/select/i", $query)) {
+            $result = array();
+            $table = array();
+            $field = array();
+            $tempResults = array();
+            $numOfFields = 0;
+            while ($fieldinfo = mysqli_fetch_field($this->_result)) {
+                array_push($table, $fieldinfo->table);
+                array_push($field, $fieldinfo->name);
+                $numOfFields++;
+            }
+            while ($row = mysqli_fetch_row($this->_result)) {
+                for ($i = 0; $i < $numOfFields; ++$i) {
+                    $table[$i] = trim(ucfirst($table[$i]), "s");
+                    $tempResults[$table[$i]][$field[$i]] = $row[$i];
+                }
+                if ($singleResult == 1) {
+                    mysqli_free_result($this->_result);
+                    return $tempResults;
+                }
+                array_push($result, $tempResults);
+            }
+            mysqli_free_result($this->_result);
+            return($result);
+        }
+    }
 
-		if (preg_match("/select/i",$query)) {
-		$result = array();
-		$table = array();
-		$field = array();
-		$tempResults = array();
-		$numOfFields = mysqli_num_fields($this->_result);
-		for ($i = 0; $i < $numOfFields; ++$i) {
-		    array_push($table,mysqli_field_table($this->_result, $i));
-		    array_push($field,mysqli_field_name($this->_result, $i));
-		}
-
-		
-			while ($row = mysqli_fetch_row($this->_result)) {
-				for ($i = 0;$i < $numOfFields; ++$i) {
-					$table[$i] = trim(ucfirst($table[$i]),"s");
-					$tempResults[$table[$i]][$field[$i]] = $row[$i];
-				}
-				if ($singleResult == 1) {
-		 			mysqli_free_result($this->_result);
-					return $tempResults;
-				}
-				array_push($result,$tempResults);
-			}
-			mysqli_free_result($this->_result);
-			return($result);
-		}
-		
-
-	}
-
-    /** Get number of rows **/
+    /** Get number of rows * */
     function getNumRows() {
         return mysqli_num_rows($this->_result);
     }
 
-    /** Free resources allocated by a query **/
-
+    /** Free resources allocated by a query * */
     function freeResult() {
         mysqli_free_result($this->_result);
     }
 
-    /** Get error string **/
-
+    /** Get error string * */
     function getError() {
         return mysqli_error($this->_dbHandle);
     }
+
 }
